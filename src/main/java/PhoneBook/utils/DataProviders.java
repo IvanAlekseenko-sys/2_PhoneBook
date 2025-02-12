236b5f5aab2973d56a9987bf78aa0b93d36adf02
Ivan <ivanalekseenko.post@gmail.com>
2025-02-12 12:16:43 +0100

    Initial commit

---
+++ src/main/java/PhoneBook/utils/DataProviders.java
@@ -0,0 +1,48 @@
+package PhoneBook.utils;
+
+import PhoneBook.data.UserData;
+import PhoneBook.model.Contact;
+import org.testng.annotations.DataProvider;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class DataProviders {
+    @DataProvider
+    public static Object[][] loginDataProvider() {
+        return new Object[][]{
+                {UserData.VALID_EMAIL, UserData.VALID_PASSWORD},
+                {"portishead2026@gmail.com", "Password@1"}
+        };
+    }
+
+    @DataProvider
+    public Object[][] contactDataProvider() {
+        return new Object[][]{
+                {"name1", "lastName1", "1234567890", "admin1@gmail.com", "Germany, Berlin1", "Some Description1"},
+                {"name2", "lastName2", "1234567891", "admin2@gmail.com", "Germany, Berlin2", "Some Description2"},
+                {"name3", "lastName3", "1234567892", "admin3@gmail.com", "Germany, Berlin3", "Some Description3"}
+        };
+    }
+
+    @DataProvider
+    public Iterator<Object[]> iteratorDataProvider() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{"Name1", "LastName1", "1234567890", "admin1@gmail.com", "Germany, Berlin1", "Description1"});
+        list.add(new Object[]{"Name2", "LastName2", "1234567891", "admin2@gmail.com", "Germany, Berlin2", "Description2"});
+        list.add(new Object[]{"Name3", "LastName3", "1234567892", "admin3@gmail.com", "Germany, Berlin3", "Description3"});
+        list.add(new Object[]{"Name4", "LastName4", "1234567893", "admin4@gmail.com", "Germany, Berlin4", "Description4"});
+        return list.iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> objectDataProvider() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{new Contact().setName("Name1").setLastName("LastName1").setPhone("1234567890").setEmail("admin1@gmail.com").setAddress("Germany, Berlin1").setDescription("Description1")});
+        list.add(new Object[]{new Contact().setName("Name2").setLastName("LastName2").setPhone("1234567891").setEmail("admin2@gmail.com").setAddress("Germany, Berlin2").setDescription("Description2")});
+        list.add(new Object[]{new Contact().setName("Name3").setLastName("LastName3").setPhone("1234567892").setEmail("admin3@gmail.com").setAddress("Germany, Berlin3").setDescription("Description3")});
+        list.add(new Object[]{new Contact().setName("Name4").setLastName("LastName4").setPhone("1234567893").setEmail("admin4@gmail.com").setAddress("Germany, Berlin4").setDescription("Description4")});
+        return list.iterator();
+    }
+}
