7fa63b43c159481e3a38127c89ba8cb071d3bb94
Ivan <ivanalekseenko.post@gmail.com>
2025-03-11 14:04:21 +0100

    hw Jenkins upd

--- src/test/java/PhoneBook/LoginTests.java
+++ src/test/java/PhoneBook/LoginTests.java
@@ -70,7 +70,7 @@ public class LoginTests extends TestBase {
 
     @AfterMethod
     public void postCondition() {
-        System.out.println("Jenkins check");
+        System.out.println("Jenkins check222");
         //app.getUserHelper().clickOnSignOutButton();
     }
 }
